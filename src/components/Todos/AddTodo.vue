eec78f3498eee0d16eaf62433c898ec5fc073bc6
berkaycimsir <berkaycimsir2003@hotmail.com>
2022-09-15 18:18:30 +0300

    add todo input - design improvements, info text

--- src/components/Todos/AddTodo.vue
+++ src/components/Todos/AddTodo.vue
@@ -33,15 +33,20 @@ const addTodo = async () => {
 </script>
 
 <template>
-  <input
-    id="todo-text"
-    name="todo-text"
-    @keyup.enter="addTodo"
-    type="text"
-    v-model="text"
-    autocomplete="off"
-    required
-    class="relative block w-full appearance-none rounded-xl border-gray-200 font-medium text-md shadow-sm px-6 py-4 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-400 focus:border-2 focus:outline-none focus:ring-0"
-    placeholder="Add todo"
-  />
+  <div class="flex flex-col w-full">
+    <input
+      id="todo-text"
+      name="todo-text"
+      @keyup.enter="addTodo"
+      type="text"
+      v-model="text"
+      autocomplete="off"
+      required
+      class="relative block w-full appearance-none rounded-xl border-gray-200 font-medium text-md shadow-sm px-4 py-3 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-400 focus:border-1 focus:outline-none focus:ring-0"
+      placeholder="What are you planning to do"
+    />
+    <div class="text-sm ml-1 mt-2 text-stone-500">
+      Press enter to add your todo after typing it
+    </div>
+  </div>
 </template>
